b81227304b8f18ee6f1b9a281a94edb82a9a3ec6
meshi <mehhi@yandex.ru>
2023-02-15 22:32:30 +0300

    chat

---
+++ html-forms/chat/task.js
@@ -0,0 +1,52 @@
+const msgContainer = document.querySelector('.chat-widget__messages-container');
+const randomMsg = ['first', 'second', 'third', 'fourth', ' fifth', 'sixth', 'sevent'];
+const scrollMsgContainer = () => msgContainer.scrollTo(0, msgContainer.scrollHeight);
+
+const generateCurrentTime = () => `${new Date().getHours().length === 1 ? '0' + new Date().getHours() : new Date().getHours()}:${(new Date().getMinutes().length === 1 ? '0' + new Date().getMinutes() : new Date().getMinutes())}`
+
+const initiateTimeoutMsgListener = function() {
+  const intervalId = setInterval(() => {
+    if (document.getElementsByClassName('chat-widget_active').length) {
+      msgContainer.innerHTML +=`
+      <div class="message">
+        <div class="message__time">${generateCurrentTime()}</div>
+        <div class="message__text">Не пишешь...</div>
+      </div>`;
+      scrollMsgContainer();
+    }
+  }  
+  , 30000)
+
+  return intervalId;
+};
+
+let intervalId = initiateTimeoutMsgListener();
+
+document.querySelector('.chat-widget').addEventListener('click', function(event) {
+  event.target.closest('.chat-widget').classList.add('chat-widget_active');
+});
+
+document.querySelector('.chat-widget__input').addEventListener('keydown', function(event) {
+  if (event.keyCode === 13 && event.target.value) {
+    clearInterval(intervalId);
+    intervalId = initiateTimeoutMsgListener();
+    msgContainer.innerHTML +=   `
+    <div class="message message_client">
+      <div class="message__time">${generateCurrentTime()}</div>
+      <div class="message__text">
+        ${event.target.value}
+      </div>
+    </div>`;
+
+    msgContainer.innerHTML +=`
+    <div class="message">
+      <div class="message__time">${generateCurrentTime()}</div>
+      <div class="message__text">${randomMsg[Math.floor(Math.random() * randomMsg.length)]}</div>
+    </div>`
+
+    scrollMsgContainer();
+  };
+})
+
+
+
